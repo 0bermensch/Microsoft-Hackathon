02a598988cd6ec41923de8ff236c381048b70c3e
Shelby Mitchell <shelbzmitchell@gmail.com>
2020-04-29 02:54:31 -0700

    modal styled

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactModal from "react-modal";
+import modalx from "../assets/icons/x-big.png";
 
 export default class Modal extends Component {
   render() {
@@ -7,92 +8,125 @@ export default class Modal extends Component {
     return (
       <div className="modal">
         <button onClick={this.props.handleOpenModal}>Create New</button>
-        <ReactModal ariaHideApp={false} isOpen={this.props.showModal}>
-          {/* Modal.setAppElement(el) */}
-          <div className="modal__top">
-            <h1 className="modal__header">Create New Task</h1>
-            <button
-              className="modal__close"
-              onClick={this.props.handleCloseModal}
+        <ReactModal
+          className="modal__popup"
+          overlayClassName="modal__overlay"
+          ariaHideApp={false}
+          isOpen={this.props.showModal}
+        >
+          <div className="modal__content">
+            {/* Modal.setAppElement(el) */}
+            <div className="modal__top">
+              <h1 className="modal__header">Create New Task</h1>
+              <img
+                className="modal__close"
+                onClick={this.props.handleCloseModal}
+                src={modalx}
+              />
+            </div>
+            <form
+              className="modal__form"
+              onSubmit={this.props.handleTaskSubmit}
             >
-              Close
-            </button>
+              <h4 className="modal__form-label">Title</h4>
+              <input
+                className="modal__title"
+                name="title"
+                type="text"
+                placeholder="Enter a title for this task"
+              />
+
+              <div className="modal__select">
+                <h4 className="modal__form-label">Type of Task</h4>
+                <select
+                  name="type"
+                  className="modal__dropdown"
+                  onChange={this.props.handleSelectedType}
+                >
+                  <option>None Assigned</option>
+                  <option value="Art Assets">Art Assets</option>
+                  <option value="Bug Fix">Bug Fix</option>
+                  <option value="Miscellaneous">Miscellaneous</option>
+                </select>
+              </div>
+              <div className="modal__select">
+                <h4 className="modal__form-label">
+                  Assign a Team Member to this task
+                </h4>
+                <select
+                  onChange={this.props.handleSelectedUser}
+                  name="owner"
+                  className="modal__dropdown"
+                >
+                  <option>None Assigned</option>
+                  {this.props.users.map((user) => {
+                    return <option>{user.username}</option>;
+                  })}
+                </select>
+              </div>
+              <div className="modal__select">
+                <h4 className="modal__form-label">Assign a Priority</h4>
+                <select
+                  className="modal__dropdown"
+                  onChange={this.props.handleSelectedPriority}
+                  name="priority"
+                >
+                  <option>None Assigned</option>
+                  <option value="Low">Low</option>
+                  <option value="Medium">Medium</option>
+                  <option value="High">High</option>
+                </select>
+              </div>
+              <div className="modal__select">
+                <h4 className="modal__form-label">What is the Task Status</h4>
+                <select
+                  name="status"
+                  className="modal__dropdown"
+                  onChange={this.props.handleSelectedStatus}
+                >
+                  <option>None Assigned</option>
+                  <option value="To Do">To Do</option>
+                  <option value="In Progress">In Progress</option>
+                  <option value="Submitted">Submitted</option>
+                </select>
+              </div>
+              <div className="modal__select">
+                <h4 className="modal__form-label">Timer</h4>
+                <input
+                  className="modal__dropdown"
+                  type="text"
+                  name="timer"
+                  placeholder="2:00"
+                />
+              </div>
+              <div className="modal__select">
+                <h4 className="modal__form-label">Start Time</h4>
+                <input
+                  className="modal__dropdown"
+                  type="text"
+                  name="starttime"
+                  placeholder="9:00"
+                />
+              </div>
+              <div className="modal__buttons-container">
+                <div className="modal__buttons">
+                  <button
+                    className="modal__button--cancel"
+                    onClick={this.props.handleCloseModal}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="modal__button--save"
+                    // className="modal__button--save"
+                    type="submit"
+                  >
+                    Save
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <form className="modal__form" onSubmit={this.props.handleTaskSubmit}>
-            <h4 className="modal__form-label">Title</h4>
-            <input
-              className=""
-              name="title"
-              type="text"
-              placeholder="Enter a title for this task"
-            />
-            <button onClick={this.props.handleCloseModal}>Close</button>
-            <div>
-              <h4 className="modal__form-label">Type of Task</h4>
-              <select
-                name="type"
-                className="modal__dropdown"
-                onChange={this.props.handleSelectedType}
-              >
-                <option>None Assigned</option>
-                <option value="Bug Fix">Bug Fix</option>
-                <option value="Miscellaneous">Miscellaneous</option>
-              </select>
-            </div>
-            <div>
-              <h4 className="modal__form-label">
-                Assign a Team Member to this task
-              </h4>
-              <select
-                onChange={this.props.handleSelectedUser}
-                name="owner"
-                className="modal__dropdown"
-              >
-                <option>None Assigned</option>
-                {this.props.users.map((user) => {
-                  return <option>{user.username}</option>;
-                })}
-              </select>
-            </div>
-            <div>
-              <h4 className="modal__form-label">Assign a Priority</h4>
-              <select
-                className="modal__dropdown"
-                onChange={this.props.handleSelectedPriority}
-                name="priority"
-              >
-                <option>None Assigned</option>
-                <option value="Low">Low</option>
-                <option value="Medium">Medium</option>
-                <option value="High">High</option>
-              </select>
-            </div>
-            <div>
-              <h4 className="modal__form-label">What is the Task Status</h4>
-              <select
-                name="status"
-                className="modal__dropdown"
-                onChange={this.props.handleSelectedStatus}
-              >
-                <option>None Assigned</option>
-                <option value="To Do">To Do</option>
-                <option value="In Progress">In Progress</option>
-                <option value="Submitted">Submitted</option>
-              </select>
-            </div>
-            <div>
-              <h4 className="modal__form-label">Timer</h4>
-              <input type="text" name="timer" placeholder="2:00" />
-            </div>
-            <div>
-              <h4 className="modal__form-label">Start Time</h4>
-              <input type="text" name="starttime" placeholder="9:00" />
-            </div>
-            <div>
-              <button onClick={this.props.handleCloseModal}>Cancel</button>
-              <button>Save</button>
-            </div>
-          </form>
         </ReactModal>
       </div>
     );
